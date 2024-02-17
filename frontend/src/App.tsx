3bb52897903975944301a5f0e453498f876d8882
shabbir <kaderishabbir@gmail.com>
2024-02-17 21:12:50 +0530

    Added frontend, update lookup mime-types

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Landing } from "./components/landing";
+
+function App() {
+  return (
+    <>
+     <div className="bg-black space-x-2">
+     <div className="font-bold bg-black text-white text-4xl flex justify-center  ">
+          <span>SwiftServe</span>
+        </div>
+      
+        <Landing />
+
+       
+        
+
+     </div>
+        
+       
+          
+    
+      
+    </>
+  );
+}
+
+export default App;
